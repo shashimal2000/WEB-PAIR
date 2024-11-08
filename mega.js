052aa4cfe5171628105328dfb876fe7b3dd2d62a
shashimal2000 <115067658+shashimal2000@users.noreply.github.com>
2024-11-08 09:17:01 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'EnterYourMegaEmail',
-    password: 'EnterYourMegaPassword',
+    email: 'kodipa4728@opposir.com',
+    password: 'Rio2000@',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
